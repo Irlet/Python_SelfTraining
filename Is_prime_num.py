8792798f80ec4dd474245258604950280259968e
Elzbieta Tatoj <tatojelzbieta@gmail.com>
2020-07-02 11:59:24 +0200

    Prime_numbers_less_than Code Review, Is_prime_num New Solution added

--- Is_prime_num.py
+++ Is_prime_num.py
@@ -28,3 +28,22 @@ def is_prime2(n):
 
 print(is_prime2(122))
 print(is_prime2(11))
+
+# Another, faster & professional way:
+
+
+def is_prime_number(num):
+    if not isinstance(num, int) or num < 2:
+        return False
+    for number in range(2, int((num/2)+1)):
+        if num % number == 0:
+            return False
+    return True
+
+
+print(is_prime_number(3.5))
+print(is_prime_number(0))
+print(is_prime_number(-2))
+print(is_prime_number(97))
+print(is_prime_number(113))
+print(is_prime_number("g"))
--- Prime_numbers_less_than.py
+++ Prime_numbers_less_than.py
@@ -1,14 +1,14 @@
-def return_prime_numbers_less_than_100():
-    """Returns a list containing prime numbers that are less than 100"""
+def return_prime_numbers_less_than_(max_num):
+    """Returns a list containing prime numbers that are less than max_num"""
 
-    numbers = list(range(2, 100))
-    prime_numbers = list(range(2, 100))
+    numbers = list(range(2, max_num))
+    prime_numbers = list(range(2, max_num))
     for number in numbers:
-        for element in range(2, number):
-            if number % element == 0 and number in prime_numbers:
+        for element in range(2, int((number/2)+1)):
+            if number % element == 0:
                 prime_numbers.remove(number)
-
+                break
     return prime_numbers
 
 
-print(return_prime_numbers_less_than_100())
+print(return_prime_numbers_less_than_(113))
